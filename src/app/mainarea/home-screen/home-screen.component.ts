7798e330e811647557f2e5a3721c1203e5ce6594
sandeep application <hash.sandeep@gmail.com>
2021-07-07 04:08:11 +0530

    yes

--- src/app/mainarea/home-screen/home-screen.component.ts
+++ src/app/mainarea/home-screen/home-screen.component.ts
@@ -11,60 +11,60 @@ import { MainAperanceAppInfoService } from '../service/main-aperance-app-info.se
 })
 export class HomeScreenComponent implements OnInit {
 
-  backgroundColour: string = '#444';
-  fontColour: string = '#fff';
-  fontFamily: string = 'Arial';
-  fontSize: string = '14px';
-  appText: string = 'abc';
-  appName: string = 'abc';
-  dashboardHeaderText: string = 'abc';
+  backgroundColour = '#444';
+  fontColour = '#fff';
+  fontFamily = 'Arial';
+  fontSize = '14px';
+  appText = 'abc';
+  appName = 'abc';
+  dashboardHeaderText = 'abc';
 
-  //section
-  sectionColour: string = '#fff';
-  sectionHeadingColour: string = '#444';
-  sectionTextColour: string = '#444';
-  sectionButtonColour: string = '#444';
+  // section
+  sectionColour = '#fff';
+  sectionHeadingColour = '#444';
+  sectionTextColour = '#444';
+  sectionButtonColour = '#444';
 
-  //banner
-  isBanner: boolean = true;
-  bannerText: string = 'abc';
-  bannerColour: string = '#444';
-  bannerSize: string = '14px';
-  bannerFamily: string = 'Arial';
-  bannerAllign: string = 'center';
+  // banner
+  isBanner = true;
+  bannerText = 'abc';
+  bannerColour = '#444';
+  bannerSize = '14px';
+  bannerFamily = 'Arial';
+  bannerAllign = 'center';
 
-  //product
-  isProduct: boolean = true;
-  productCount: string = '6';
-  productFontColour: string = '#444';
-  productShape: string = 'sharp';
+  // product
+  isProduct = true;
+  productCount = '6';
+  productFontColour = '#444';
+  productShape = 'sharp';
 
 
-  isNewProduct: boolean = true;
-  isCart: boolean = true;
-  isFeatureProduct: boolean = true;
-  isRecentyProduct: boolean = true;
-  isSaleProduct: boolean = true;
+  isNewProduct = true;
+  isCart = true;
+  isFeatureProduct = true;
+  isRecentyProduct = true;
+  isSaleProduct = true;
 
   sizeList: string[] = ['12px', '13px', '14px', '15px'];
   fontFamilyList: string[] = ['Arial', 'Arial Black', 'Poppins SemiBold', 'Sans Serif', 'Times New Roman', 'Verdana'];
   columnList: string[] = ['1', '2', '3', '4',  '6'];
   categoryList: string[] = ['1', '2', '3', '4', '5', '6'];
 
-  constructor(private mainAppService: MainAperanceAppInfoService, private appService: AppService,private router:Router) {
+  constructor(private mainAppService: MainAperanceAppInfoService, private appService: AppService, private router: Router) {
     this.appName = appService.selectedApp.Appname;
   }
 
   ngOnInit() {
-    let obj = this.mainAppService.getDataHomeScreen();
+    const obj = this.mainAppService.getDataHomeScreen();
     if (obj) {
-      //section
+      // section
       this.sectionColour = obj.sectionColor.sectionColour;
       this.sectionHeadingColour = obj.sectionColor.sectionHeadingColour;
       this.sectionTextColour = obj.sectionColor.sectionTextColour;
       this.sectionButtonColour = obj.sectionColor.sectionButtonColour;
 
-      //banner
+      // banner
       this.isBanner = obj.mainBanner.isBanner;
       this.bannerText = obj.mainBanner.bannerText;
       this.bannerColour = obj.mainBanner.bannerColour;
@@ -72,7 +72,7 @@ export class HomeScreenComponent implements OnInit {
       this.bannerFamily = obj.mainBanner.bannerFamily;
       this.bannerAllign = obj.mainBanner.bannerAllign;
 
-      //product
+      // product
       this.isProduct = obj.productCategories.isProduct;
       this.productCount = obj.productCategories.productCount;
       this.productFontColour = obj.productCategories.productFontColour;
@@ -88,9 +88,9 @@ export class HomeScreenComponent implements OnInit {
   }
 
   saveData() {
-    let obj: HomeScreenAttributeModel = new HomeScreenAttributeModel();
-    //section
-    let sectionColor: SectionHomeScreenModel = new SectionHomeScreenModel();
+    const obj: HomeScreenAttributeModel = new HomeScreenAttributeModel();
+    // section
+    const sectionColor: SectionHomeScreenModel = new SectionHomeScreenModel();
     sectionColor.sectionColour = this.sectionColour;
     sectionColor.sectionHeadingColour = this.sectionHeadingColour;
     sectionColor.sectionTextColour = this.sectionTextColour;
@@ -98,8 +98,8 @@ export class HomeScreenComponent implements OnInit {
 
     obj.sectionColor = sectionColor;
 
-    //banner
-    let mainBanner: MainBannerHomeScreenModel = new MainBannerHomeScreenModel();
+    // banner
+    const mainBanner: MainBannerHomeScreenModel = new MainBannerHomeScreenModel();
     mainBanner.isBanner = this.isBanner;
     mainBanner.bannerText = this.bannerText;
     mainBanner.bannerColour = this.bannerColour;
@@ -109,8 +109,8 @@ export class HomeScreenComponent implements OnInit {
 
     obj.mainBanner = mainBanner;
 
-    //product
-    let productCategories: ProductHomeScreenModel = new ProductHomeScreenModel();
+    // product
+    const productCategories: ProductHomeScreenModel = new ProductHomeScreenModel();
     productCategories.isProduct = this.isProduct;
     productCategories.productCount = this.productCount;
     productCategories.productFontColour = this.productFontColour;
@@ -118,22 +118,22 @@ export class HomeScreenComponent implements OnInit {
 
     obj.productCategories = productCategories;
 
-    obj.isNewProduct=this.isNewProduct;
-    obj.isCart=this.isCart;
-    obj.isFeatureProduct=this.isFeatureProduct;
-    obj.isRecentyProduct=this.isRecentyProduct;
-    obj.isSaleProduct=this.isSaleProduct;
+    obj.isNewProduct = this.isNewProduct;
+    obj.isCart = this.isCart;
+    obj.isFeatureProduct = this.isFeatureProduct;
+    obj.isRecentyProduct = this.isRecentyProduct;
+    obj.isSaleProduct = this.isSaleProduct;
     this.mainAppService.setDataHomeScreen(obj);
 
-    let responseObj:AppAttributeModel=new AppAttributeModel();
-    responseObj.appID=this.appService.selectedApp._id;
-    responseObj.homeAttr=[obj];
-    responseObj.launchscreenAttr=this.mainAppService.getDataLaunch() !=null ?[this.mainAppService.getDataLaunch()]:[];
-    responseObj.appiconAttr=this.mainAppService.getDataMainArea() !=null ?[this.mainAppService.getDataMainArea()]:[];
-    responseObj.iconimage='';
-    responseObj.launchimage='';
+    const responseObj: AppAttributeModel = new AppAttributeModel();
+    responseObj.appID = this.appService.selectedApp._id;
+    responseObj.homeAttr = [obj];
+    responseObj.launchscreenAttr = this.mainAppService.getDataLaunch() != null ? [this.mainAppService.getDataLaunch()] : [];
+    responseObj.appiconAttr = this.mainAppService.getDataMainArea() != null ? [this.mainAppService.getDataMainArea()] : [];
+    responseObj.iconimage = '';
+    responseObj.launchimage = '';
 
-    this.appService.addAppAttribute(responseObj).subscribe(v=>{
+    this.appService.addAppAttribute(responseObj).subscribe(v => {
 
     });
   }
