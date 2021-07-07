c8fc2cb12d7cd0980669aac22d84c1a5916b4df6
sandeep application <hash.sandeep@gmail.com>
2021-07-07 15:52:25 +0530

    Merge branch 'master' of https://github.com/sandeepcore/AppMySite

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
      { path:'login', loadChildren: () => import('./login/login.module').then(m =>m.LoginModule)},
      { path:'', loadChildren: () => import('./dashboard/dashboard.module').then(m =>m.DashboardModule),canActivate:[AuthGuard]},
    // {path: '', component: AppMainComponent,},
-    { path: 'order', loadChildren: () => import('./../app/order-list/order-list.module').then(m => m.OrderListModule),canActivate:[AuthGuard] },
+    { path: 'product', loadChildren: () => import('./../app/order-list/order-list.module').then(m => m.OrderListModule),canActivate:[AuthGuard] },
     { path: 'appearance', loadChildren: () => import('./mainarea/mainarea.module').then(m => m.MainareaModule),canActivate:[AuthGuard] },
     { path: 'step', loadChildren: () => import('./step/step.module').then(m => m.StepModule),canActivate:[AuthGuard] },
     { path:'', redirectTo:'login', pathMatch:'full'}
---
+++ src/app/order-list/component/category-inventory/category-inventory.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-inventory',
+  templateUrl: './category-inventory.component.html',
+  styleUrls: ['./category-inventory.component.scss']
+})
+export class CategoryInventoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
