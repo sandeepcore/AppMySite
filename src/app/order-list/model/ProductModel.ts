c0e97b1540e96eebe7bf585919b66401f4fb78fe
prateek <prateek@zibal.com>
2021-07-11 00:19:26 +0530

    product model update

--- src/app/order-list/model/ProductModel.ts
+++ src/app/order-list/model/ProductModel.ts
@@ -3,9 +3,9 @@ export class ProductModel{
         keywords:string;
         description:string;
         content:string;
-        parentCatid:string;
-        subCatid:string;
-        childCatId:string;
+        parentCatid:string="";
+        subCatid:string="";
+        childCatId:string="";
         imageUrl:string[];
         price:string;
         tax:string;
